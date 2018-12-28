35b2562a7e4164144208ebf50464d652b478dd3b
nicaisa <1010114387@qq.com>
2018-12-28 08:06:20 +0000

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
+    /*
+     *用戶主頁
+     * */
+    public function index(User $user)
+    {
+        return view('users.index', compact('user'));
+    }
+
+
+    /*
+     * 註冊
+     * */
     public function create()
     {
         return view('users.create');
+    }
+
+    /*
+     * 用戶信息
+     * */
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
     }
 }
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+
+@section('title', '主页')
+
+@section('content')
+    <h1>用戶主頁</h1>
+@stop
