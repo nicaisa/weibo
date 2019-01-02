46a507b61eccc46b30f67952eb6b9953cf65e983
nicaisa <1010114387@qq.com>
2019-01-02 06:45:51 +0000

    登录成功后导航逻辑

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,18 +2,21 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Weibo App')- Laravel 新手入门教程</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
     @include('layouts._header')
 
-    <div class="container">
+    <div class="container" id="app">
         <div class="offset-md-1 col-md-10">
             @include('shared._messages')
             @yield('content')
             @include('layouts._footer')
         </div>
     </div>
+
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
