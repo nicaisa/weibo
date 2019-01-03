c493b9e70c7bd5752333402b2c5036816be2332e
nicaisa <1010114387@qq.com>
2019-01-03 08:21:37 +0000

    用户微博列表

---
+++ resources/views/statuses/_status.blade.php
@@ -0,0 +1,9 @@
+<li class="media mt-4 mb-4">
+    <a href="{{ route('users.show', $user->id) }}">
+        <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}"  class="mr-3 gravatar"/>
+    </a>
+    <div  class="media-body">
+        <h5 class="mt-0 mb-1">{{ $user->name }} <samll>/ {{ $status->created_at->diffForHumans() }}</samll></h5>
+        {{ $status->content }}
+    </div>
+</li>
